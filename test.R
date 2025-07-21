0cf9caaa59d20336ff96148e6f39b5ceb5e37ab0
Chiara Biow <cbiow@bwh.harvard.edu>
2025-07-20 21:32:03 -0400

    added how to link rstudio to a github account
    and how to connect a specific project to github

--- test.R
+++ test.R
@@ -4,3 +4,19 @@ library(tidyverse)
 
 install.packages("janitor")
 library(janitor)
+
+#so far we have looked at how to use git locally
+#what about if we want to link rstudio git to github?
+
+#THIS CONNECTS THIS RSTUDIO TO GITHUB
+library(usethis)
+create_github_token()
+
+library(gitcreds)
+gitcreds_set()
+
+#adding these credentials will allow me to sink between rstudio and github 
+
+#TO CONNECT THIS SPECIFIC PROJECT 
+#will create a repository called testrepo
+use_github()
